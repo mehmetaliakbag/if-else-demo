d13b55049d79b3b4d6164e395464bbf5ce0cb941
Mehmet Ali <Mehmet Ali@DESKTOP-4TIU0GP>
2024-10-07 01:07:06 +0300

    Add project files.

---
+++ IfElse/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Bir sayı girin: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+}
+else if (num < 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+}
+else
+    Console.WriteLine("Girilen sayı 10'dur.");
+
+if (num > 0)
+{
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("Girilen sayı çifttir.");
+    }
+    else
+    {
+        Console.WriteLine("Girilen sayı tektir");
+    }
+}
+else if (num < 0)
+{
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("Girilen sayı çifttir.");
+    }
+    else
+    {
+        Console.WriteLine("Girilen sayı tektir");
+    }
+}
+else
+{
+    Console.WriteLine("Girilen sayı sıfırdır.");
+}
+
